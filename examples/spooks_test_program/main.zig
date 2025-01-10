07bb523fd92844b57161c5fb3bc3af32b14f332e
Kole <contact@koles.tech>
2025-01-10 11:35:34 +0000

    Add allocators

--- examples/spooks_test_program/main.zig
+++ examples/spooks_test_program/main.zig
@@ -4,6 +4,11 @@ const wnd = @import("binslib").wnd;
 const draw = @import("binslib").draw;
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var temp_allocator = std.heap.ArenaAllocator.init(gpa.allocator());
+    // I just threw in this reset here to shut up the "unchanged var" whinging
+    _ = temp_allocator.reset(.retain_capacity);
+
     try wnd.init(800, 600, "binslib");
     defer wnd.deinit();
     try draw.init();
