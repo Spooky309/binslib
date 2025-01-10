1b41049dd63b3c08b78b4085b0a6b63c10f36707
Kole <contact@koles.tech>
2025-01-10 11:44:30 +0000

    add gonzos test program

---
+++ examples/gonzos_test_program/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+const wnd = @import("binslib").wnd;
+const draw = @import("binslib").draw;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var temp_allocator = std.heap.ArenaAllocator.init(gpa.allocator());
+
+    try wnd.init(800, 600, "binslib");
+    defer wnd.deinit();
+    try draw.init();
+    defer draw.deinit();
+    while (!wnd.wants_close()) {
+        wnd.pump();
+        draw.begin_frame();
+        draw.end_frame();
+
+        // We don't care if this fails.
+        _ = temp_allocator.reset(.retain_capacity);
+    }
+}
