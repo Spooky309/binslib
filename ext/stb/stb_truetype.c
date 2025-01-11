1bdb93f8ef6cf7da5a71efc96dc1abb3166c7325
Thomas gove <>
2025-01-11 22:05:06 +0000

    Added stb_truetype and stb_rect_pack

---
+++ ext/stb/stb_truetype.c
@@ -0,0 +1,3 @@
+#define STB_RECT_PACK_IMPLEMENTATION
+#define STB_TRUE_TYPE_IMPLEMENTATION
+#include "stbtt_with_stbrp.h"
---
+++ ext/stb/stbtt_with_stbrp.h
@@ -0,0 +1,2 @@
+#include "stb_rect_pack.h" // Must be included before stb_truetype
+#include "stb_truetype.h"
